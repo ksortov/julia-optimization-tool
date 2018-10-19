dacd72c1840baf2205ac90736bfb22c655af41c3
Kevin Sorto-Ventura <ksortoventura@gmail.com>
2018-10-19 19:47:17 -0400

    Add constraints to the model

--- Optimization Tool/opt_tool_draft.jl
+++ Optimization Tool/opt_tool_draft.jl
@@ -1,13 +1,13 @@
-using JuMP
-using Ipopt
+using JuMP, Ipopt, Clp
 
 m = Model(solver = IpoptSolver()) # define IpoptSolver as solver (placeholder for now)
 
 # define sets
-N = 4 # total number of nodes
+N = 1 # total number of nodes
 T = 1 # largest time value (hour)
 V = 1 # number of possible voltage levels
 L = Array{Int64}(4, 4) # array of possible links (L(n,m) = 1 if there is a link b/w n % m)
+A = 100 # large number used for a constraint on the dummy variable
 
 # define parameters
 d_nm = Array{Float64}(N, N) # distances between nodes n & m (km)
@@ -26,9 +26,9 @@ c_n = Array{Float64}(1, N) # cost of adding genration @ node n ($)
 # define problem variables
 @variable(m, x_nm[1:N, 1:N], Int) # number of parallel lines b/w nodes n & m
 @variable(m, y_v[1:V], Bin) # boolean for selected voltage
-@variable(m, g_nt[1:N, 1:T]) # generation injection @ node n & time t (MW)
-@variable(m, p_nmt[1:N, 1:N, 1:T]) # power flow b/w nodes n & m @ time t (MW)
-@variable(m, u_nt[1:N, 1:T]) # voltage @ node n & time t (kV)
+@variable(m, g_nt[1:N, 1:T], Float64) # generation injection @ node n & time t (MW)
+@variable(m, p_nmt[1:N, 1:N, 1:T], Float64) # power flow b/w nodes n & m @ time t (MW)
+@variable(m, u_nt[1:N, 1:T], Float64) # voltage @ node n & time t (kV)
 @variable(m, z_n[1:N], Bin) # boolean for building generation site @ node n
 @variable(m, alpha_vnm[1:V, 1:N, 1:N], Int) # dummy variable for linearization
 
@@ -50,12 +50,16 @@ c_n = Array{Float64}(1, N) # cost of adding genration @ node n ($)
 #@NLobjective(m, Min, links + subs + gens + ops) # define function to optimize
 
 # adding constraints
-@constraint(m, (g_nt - del_nt - sum(p_nmt[n,m,t] for m = 1:N) for n = 1:N for t = 1:T) == 0.0)
-@constraint(m,)
-@constraint(m,)
-@constraint(m,)
+@constraint(m, (g_nt[n,t] - del_nt[n,t] - sum(p_nmt[n,m,t] for m = 1:N) for n = 1:N for t = 1:T) == 0.0)
+@constraint(m, (p_nmt[n,m,t] for n = 1:N for m = 1:N for t = 1:T if n !=m) ==
+(sum((alpha_vnm[v,n,m]/r_v[v])*(u_nt[n,t] - u_nt[m,t])*u_nt[n,t] for v = 1:V)))
+@constraint(m, (-x_nm[n,m]*p_v[v]) <= (p_nmt[n,m,t] for n = 1:N for m = 1:N for v = 1:V for t = 1:t) <= (x_nm[n,m]*p_v[v]))
+@constraint(m,(y_v[v]*fmin*f_v[v] for v = 1:V) <= (y_v[v]*fmax*f_v[v]))
 @constraint(m, sum(y_v[v] for v = 1:V) == 1)
-@constraint(m,)
+@constraint(m, (alpha_vnm[v,n,m] for n = 1:N for m = 1:N for v = 1:V) == (y_v[v]*x_nm[n,m]))
+@constraint(m, 0 <= (alpha_vnm[v,n,m] for n = 1:N for m = 1:N for v = 1:V) <= (A*y_v[v]))
+@constraint(m, (A*-(1 - y_v[v]) + x_nm[n,m] for n = 1:N for m = 1:N for v = 1:V) <= (alpha_vnm[v,n,m]))
+@constraint(m, (alpha_vnm[v,n,m] for n = 1:N for m = 1:N for v = 1:V) <= (x_nm[n,m] + A*(1 - y_v[v])))
 
 solve(m)
 
