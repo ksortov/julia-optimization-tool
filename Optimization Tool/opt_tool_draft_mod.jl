db7013f3b0e0a0d8683af9f9e77d3f67b953b4c3
Kevin Sorto-Ventura <ksortoventura@gmail.com>
2018-11-11 15:31:35 -0500

    Reduce number of variables to simplify problem

--- Optimization Tool/opt_tool_draft_mod.jl
+++ Optimization Tool/opt_tool_draft_mod.jl
@@ -16,7 +16,7 @@ N = 2 # total number of nodes
 T = 7 # largest time value (hour)
 #V = 4 # number of possible voltage levels
 #L = zeros(Int, N, N) # array of possible links (L(n,m) = 1 if there are links b/w n & m)
-L = [0 1; 1 0]
+L = [0 1; 0 0]
 #A = 100 # large number used for a constraint on the dummy variable
 
 # Define parameters
@@ -57,44 +57,32 @@ end
 fmax = 1.05 # maximum voltage value for nodes (pu)
 fmin = 0.95 # minimum voltage value for nodes (pu)
 gamma = 0.0175 # discount rate
-c_n = zeros(Float64, N, 1) # cost of adding genration @ node n ($)
+#c_n = 1e20*ones(Float64, N, 1) # cost of adding genration @ node n ($)
 
 # Define problem variables
-@variable(M, 0 <= x_nm[1:N, 1:N] <= 3, Int) # number of parallel lines b/w nodes n & m
-#@variable(M, y_v[1:V], Bin) # boolean for selected voltage
-@variable(M, g_nt[1:N, 1:T] >= 0.0) # generation injection @ node n & time t from new generation construction (MW)
-@variable(M, del_nt[1:N, 1:T] >= 0.0) # power injection @ node n & time t without new generation construction (MW)
-@variable(M, p_nmt[1:N, 1:N, 1:T]) # power flow b/w nodes n & m @ time t (MW)
-@variable(M, u_nt[1:N, 1:T]) # voltage @ node n & time t (kV)
-@variable(M, 0 <= z_n[1:N] <= 1, Int) # boolean for building generation site @ node n
-#@variable(M, alpha_vnm[1:V, 1:N, 1:N], Int) # dummy variable for linearization
+@variables(M, begin
+    0 <= x_nm[1:N, 1:N] <= 3, Int # number of parallel lines b/w nodes n & m
+    #y_v[1:V], Bin) # boolean for selected voltage
+    #g_nt[1:N, 1:T] >= 0.0 # generation injection @ node n & time t from new generation construction (MW)
+    del_nt[1:N, 1:T] >= 0.0 # power injection @ node n & time t without new generation construction (MW)
+    p_nmt[1:N, 1:N, 1:T] # power flow b/w nodes n & m @ time t (MW)
+    u_nt[1:N, 1:T] # voltage @ node n & time t (kV)
+    #0.0 <= z_n[1:N] <= 1.0, Int # boolean for building generation site @ node n
+    #alpha_vnm[1:V, 1:N, 1:N], Int) # dummy variable for linearization
+end)
 
 # Define objective function to minimize
-@objective(M, Min, sum(a*p*d_nm[n,m]*x_nm[n,m] for n = 1:N for m = 1:N if n < m) # cost of links
-+ sum(b*p*x_nm[n,m] for n = 1:N for m = 1:N if n != m) # cost of substations
-+ sum(c_n[n]*z_n[n] for n = 1:N) # cost of generation construction
-+ sum(gamma^t for t = 1:T)*sum(lambda_nt[n,t]*(g_nt[n,t] + del_nt[n,t]) for n = 1:N for t = 1:T)) # cost of operations
+@objective(M, Min, sum(a*p*d_nm[n,m]*x_nm[n,m] for n in 1:N for m in 1:N if n < m) # cost of links
++ sum(b*p*x_nm[n,m] for n in 1:N for m in 1:N if n != m) # cost of substations
+#+ sum(c_n[n]*z_n[n] for n in 1:N) # cost of generation construction
++ sum(gamma^t for t in 1:T)*sum(lambda_nt[n,t]*(del_nt[n,t]) for n in 1:N for t in 1:T)) # cost of operations
 
 # Add constraints
 
-# Logical constraint on g_nt and z_n
+# Power balance at a node constraint
 for n in 1:N
     for t in 1:T
-        if z_n[n] == 0
-            @constraint(M, g_nt[n,t] == 0)
-        else
-            @constraint(M, g_nt[n,t] >= 1)
-        end
-    end
-end
-
-# Power balance constraint
-for n in 1:N
-    for t in 1:T
-        @constraint(M, (g_nt[n,t] + del_nt[n,t] - dem_nt[n,t] == sum(p_nmt[n,m,t] for m = 1:N if L[n,m] == 1 && n != m)))
-        @constraint(M, g_nt[1,t] == 0)
-        @constraint(M, g_nt[2,t] == 0)
-        #@constraint(M, del_nt[1,t] == 0)
+        @constraint(M, (del_nt[n,t] - dem_nt[n,t] == sum(p_nmt[n,m,t] for m in 1:N if n != m)))
     end
 end
 
@@ -103,7 +91,7 @@ end
 for n in 1:N
     for m in 1:N
         for t in 1:T
-            if L[n,m] == 1
+            if n != m
                 #@NLconstraint(M, (p_nmt[n,m,t]) == (sum((alpha_vnm[v,n,m]/r_v[v])*(u_nt[n,t] - u_nt[m,t])*u_nt[n,t] for v = 1:V)))
                 @NLconstraint(M, (p_nmt[n,m,t]) == ((x_nm[n,m]/r)*(u_nt[n,t] - u_nt[m,t])*u_nt[n,t]))
                 #@constraint(M, (p_nmt[m,n,t]) + (p_nmt[n,m,t]) == 0)
@@ -117,7 +105,7 @@ for n in 1:N
     for m in 1:N
         #for v in 1:V
             for t in 1:T
-                if L[n,m] == 1
+                if n != m
                     #@constraint(M, (-p_v[v]*x_nm[n,m]) <= (p_nmt[n,m,t]))
                     #@constraint(M, (p_nmt[n,m,t]) <= (p_v[v]*x_nm[n,m]))
                     @constraint(M, (-p*x_nm[n,m]) <= (p_nmt[n,m,t]))
@@ -155,9 +143,9 @@ solve(M) # solve model
 
 # Print results to console
 println("x_nm = ", getvalue(x_nm))
-println("g_nt = ", getvalue(g_nt))
-println("del_nt = ", getvalue(del_nt))
-println("p_nmt = ", getvalue(p_nmt))
-println("u_nt = ", getvalue(u_nt)/f)
-println("z_n = ", getvalue(z_n))
+#println("g_nt = ", getvalue(g_nt))
+#println("del_nt = ", getvalue(del_nt))
+#println("p_nmt = ", getvalue(p_nmt))
+#println("u_nt = ", getvalue(u_nt)/f)
+#println("z_n = ", getvalue(z_n))
 println("Objective value = ", getobjectivevalue(M))
