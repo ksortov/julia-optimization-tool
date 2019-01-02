75addc98f9dadf3d3f3d6070d24f101364eccfa9
Kevin Sorto-Ventura <ksortoventura@gmail.com>
2019-01-02 11:07:02 -0500

    Implement voltage level selection

--- Optimization Tool/design_tool_0.1.jl
+++ Optimization Tool/design_tool_0.1.jl
@@ -11,17 +11,24 @@ mod = Model(solver = AmplNLSolver("C:/Users/kevin/Desktop/Design_Project/julia-o
 ["C:/Users/kevin/Desktop/Design_Project//julia-optimization-tool/Optimization Tool/scipampl_exe/scip.set"]))
 
 # Define sets
+V = 3 # total number of potential voltage levels
 N = 2 # total number of nodes
-T = 100 # largest time value (hour)
+T = 10 # largest time value (hour)
+A = 1000 # large number used for dummy variable constraints
 L = [0 1; 1 0] # array of possible links (L[n,m] = 1 if there can be links b/w n & m)
 
 # Define parameters
 d_nm = [0 241; 241 0] # distances between nodes n & m (km)
 a = 1.21e6 # cost of links ($/MW*km)
+a_v = [1.31e6 1.21e6 1.52e6]
 b = 275e6 # cost of substation ($/MW)
+b_v = [200e6 275e6 300e6]
 r = 0.009 # resistance on link (ohm/km)
+r_v = [0.007 0.009 0.011]
 f = 500 # voltage level in (kV), also voltage base
+f_v = [315 500 735]
 p = 2407 # power capacity of a link (MW)
+p_v = [2150 2407 2789]
 dem_nt = zeros(Float64, N, T) # power demand @ node n & time t (MW)
 for n = 1:N
     for t = 1:T
@@ -57,17 +64,19 @@ c_n = 500e6*ones(Float64, N, 1) # cost of adding genration @ node n ($)
     p_nmt[1:N, 1:N, 1:T] # power flow b/w nodes n & m @ time t (MW)
     u_nt[1:N, 1:T] # voltage @ node n & time t (kV)
     z_n[1:N], Bin # boolean for new generation construction decision
+    y_v[1:V], Bin # boolean for selected voltage
+    alpha_vnm[1:V, 1:N, 1:N], Int # dummy variable for linearization
 end)
 
 # Define objective function to minimize
-@objective(mod, Min, sum(x_nm[n,m]*a*p*d_nm[n,m] for n in 1:N for m in 1:N if n < m) # cost of links
-+ sum(x_nm[n,m]*b*p for n in 1:N for m in 1:N if n != m) # cost of substations
+@objective(mod, Min, sum(alpha_vnm[v,n,m]*a_v[v]*p_v[v]*d_nm[n,m] for v in 1:V for n in 1:N for m in 1:N if n < m) # cost of links
++ sum(alpha_vnm[v,n,m]*b_v[v]*p_v[v] for v in 1:V for n in 1:N for m in 1:N if n != m) # cost of substations
 + sum(c_n[n]*z_n[n] for n in 1:N) # cost of generation construction
-+ sum(dr^t for t in 1:T)*sum(lambda_nt[n,t]*(g_nt[n,t] - del_nt[n,t]) for n in 1:N for t in 1:T)) # cost of operations
++ sum(dr^t for t in 1:T)*sum(lambda_nt[n,t]*(g_nt[n,t] + del_nt[n,t]) for n in 1:N for t in 1:T)) # cost of operations
 
 # Add constraints
 
-# Decision to add generation
+# Decision to add generation (z_n boolean)
 for n in 1:N
     for t in 1:T
         @constraint(mod, g_nt[n,t] == z_n[n]*g_nt[n,t])
@@ -88,8 +97,7 @@ for n in 1:N
     for m in 1:N
         for t in 1:T
             if L[n,m] == 1
-                @NLconstraint(mod, (p_nmt[n,m,t]) == ((x_nm[n,m]/r)*(u_nt[n,t] - u_nt[m,t])*u_nt[n,t]))
-                #@NLconstraint(mod, (p_nmt[n,m,t]) == ((x_nm[n,m]/r_v[v])*(u_nt[n,t] - u_nt[m,t])*u_nt[n,t]))
+                @NLconstraint(mod, (p_nmt[n,m,t]) == sum((alpha_vnm[v,n,m]/r_v[v])*(u_nt[n,t] - u_nt[m,t])*u_nt[n,t] for v in 1:V))
             end
         end
     end
@@ -100,8 +108,8 @@ for n in 1:N
     for m in 1:N
         for t in 1:T
             if L[n,m] == 1
-                @constraint(mod, (-p*x_nm[n,m]) <= (p_nmt[n,m,t])) # lower bound
-                @constraint(mod, (p_nmt[n,m,t]) <= (p*x_nm[n,m])) # upper bound
+                @constraint(mod, sum(-p_v[v]*x_nm[n,m] for v in 1:V) <= (p_nmt[n,m,t])) # lower bound
+                @constraint(mod, (p_nmt[n,m,t]) <= sum(p_v[v]*x_nm[n,m] for v in 1:V)) # upper bound
                 @constraint(mod, x_nm[m,n] == x_nm[n,m]) # links b/w n&m = links b/w m&n
             elseif n == m
                 @constraint(mod, x_nm[n,m] == 0) # no links between same node
@@ -114,7 +122,25 @@ end
 # Voltage bounds constraint
 for n in 1:N
     for t in 1:T
-        @constraint(mod, (fmin*f) <= (u_nt[n,t]) <= (fmax*f))
+        @constraint(mod, (u_nt[n,t]) >= sum(fmin*f_v[v]*y_v[v] for v in 1:V)) # lower bound
+        @constraint(mod, (u_nt[n,t]) <= sum(fmax*f_v[v]*y_v[v] for v in 1:V)) # upper bound
+    end
+end
+
+# Logical constraint on y_v (only one voltage level is selected)
+@constraint(mod, (sum(y_v[v] for v in 1:V)) == 1)
+
+# Constarints on linearization variable alpha
+# which is equal to x_nm when the boolean y_v is 1
+for v in 1:V
+    for n in 1:N
+        for m in 1:N
+            @NLconstraint(mod, (alpha_vnm[v,n,m]) == (y_v[v]*x_nm[n,m])) # alpha is the product of links and selected voltage boolean
+            @constraint(mod, (alpha_vnm[v,n,m]) >= 0) # minimum value is 0
+            @constraint(mod, (alpha_vnm[v,n,m]) <= (A*y_v[v])) # maximum value is a large number
+            @constraint(mod, (alpha_vnm[v,n,m]) >= (A*-(1 - y_v[v]) + x_nm[n,m])) # these two last constranits ensure that alphga
+            @constraint(mod, (alpha_vnm[v,n,m]) <= (x_nm[n,m] + A*(1 - y_v[v])))  # actually takes on the value of x_nm (links)
+        end
     end
 end
 
@@ -122,13 +148,14 @@ status = solve(mod) # solve model
 
 # Write outputs to csv files (add file path before file name)
 #CSV.write("C:/Users/kevin/Desktop/Design_Project/julia-optimization-tool/Optimization Tool/outputs/links_out.csv", DataFrame(getvalue(x_nm)), append = true)
-#CSV.write("gen_sites_out.csv", DataFrame(getvalue(z_n.')), append = true)
+#CSV.write("gen_sites_out.csv", DataFrame(getvalue(z_n)), append = true)
 
 # Print results to console
+println("y_v = ", getvalue(y_v))
 println("x_nm = ", getvalue(x_nm))
-#println("g_nt = ", getvalue(g_nt))
-#println("del_nt = ", getvalue(del_nt))
-#println("p_nmt = ", getvalue(p_nmt))
-#println("u_nt = ", getvalue(u_nt))
+println("g_nt = ", getvalue(g_nt))
+println("del_nt = ", getvalue(del_nt))
+println("p_nmt = ", getvalue(p_nmt))
+println("u_nt = ", getvalue(u_nt))
 println("z_n = ", getvalue(z_n.'))
 println("Objective value = ", getobjectivevalue(mod))
