814e7d99d17a276849f8960efd0f04592f40bb8c
apacarar <apacarar@hotmail.com>
2019-01-29 12:35:59 -0500

    add time constraints

--- PCM.jl
+++ PCM.jl
@@ -15,33 +15,39 @@ const c_g = [50,100];
 const c_g0 = [1000,0]
 # Incremental cost of wind generators
 const c_w = 50;
-# Total demand
-const d = 500;
 # Wind forecast
 const w_f = 200;
+#Largest time value
+const T=10;
 
+#Total demand at every hour
+dem=[300, 500, 700, 900, 1100, 1300, 1500, 1400, 1300, 1200];
 
 #Define the economic dispatch (ED) model
 ed=Model(solver = ClpSolver())
 
 # Define decision variables
-@variable(ed, 0 <= g[i=1:2] <= g_max[i]) # power output of generators
+@variable(ed, 0 <= g[i=1:2 t=1:T] <= g_max[i]) # power output of generators
 @variable(ed, 0 <= w  <= w_f ) # wind power injection
 
 # Define the objective function
-@objective(ed,Min,sum(c_g[i] * g[i] for i=1:2)+ c_w * w)
+@objective(ed,Min,sum(c_g[i,t] * g[i,t] for i=1:2 for t in 1:T)+ c_w * w)
 
 # Define the constraint on the maximum and minimum power output of each generator
 for i in 1:2
-    @constraint(ed,  g[i] <= g_max[i]) #maximum
-    @constraint(ed,  g[i] >= g_min[i]) #minimum
+    for t in 1:T
+        @constraint(ed,  g[i,t] <= g_max[i]) #maximum
+        @constraint(ed,  g[i,t] >= g_min[i]) #minimum
+    end
 end
 
 # Define the constraint on the wind power injection
 @constraint(ed, w <= w_f)
 
 # Define the power balance constraint
-@constraint(ed, sum(g[i] for i=1:2) + w == d)
+for t in 1:T
+    @constraint(ed, sum(g[i,t] for i=1:2) + w == dem[t])
+end
 
 # Solve statement
 solve(ed)
@@ -57,7 +63,7 @@ obj=getObjectiveValue(ed)
 #(g_opt,w_opt,ws_opt,obj) = solve_ed(g_max, g_min, c_g, c_w, d, w_f);
 
 println("\n")
-println("Dispatch of Generators: ", g_opt[i=1:2], " MW")
+println("Dispatch of Generators: ", g_opt[i=1:2,t], " MW")
 println("Dispatch of Wind: ", w_opt, " MW")
 println("Wind spillage: ", w_f-w_opt, " MW")
 println("\n")
